a7a114aede9a240adcf4cbf06a0898684105ab91
Adrien <adrien.aubourg@hotmail.fr>
2024-03-10 11:07:09 +0100

    cors test

--- public/index.php
+++ public/index.php
@@ -7,11 +7,7 @@ header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: *");
 header("Allow: *");
 
-$method = $_SERVER['REQUEST_METHOD'];
 
-if ($method === "OPTIONS") {
-    die();
-}
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
